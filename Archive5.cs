431f4cec712ba64c4e0e35916b94e7f06c8139a0
Konra <denapoliconra@gmail.com>
2025-04-07 02:35:09 -0300

    Create Archive5.cs

---
+++ Archive5.cs
@@ -0,0 +1,11 @@
+// Crea un programa en C# que no haga nada. Lo único que deberás hacer será escribir un comentario 
+// /TODO en el Main del programa y añadir un comentario de varias lineas con tu nombre
+//  y la fecha de creación en la primera linea del programa.
+
+class Ejercicio {
+    static void Main(string[] args) {
+        //TODO: Konra
+        // 30/03/2025
+        
+        }
+}
